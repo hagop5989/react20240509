1573910423518efe171f9493088326d052fd7684
daeseong <zang5989@gmail.com>
2024-05-09 11:12:22 +0900

    20240509 react 필요 없는 코드 지우기 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-// built-in component : html 의 기본 요소, 태그명이 소문자로 시작
-// component : 직접만든 요소(태그), 대문자로 시작
-// component 는 return 을 가진 함수
-
-// props
-
 function App() {
   return <div> hello react</div>;
 }
