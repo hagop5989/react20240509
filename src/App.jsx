6a3bdb8a2f9b1d225ecb85110dc9006bf977ddfc
daeseong <zang5989@gmail.com>
2024-05-09 14:16:18 +0900

    20240509 react 문법공부, &&(and ...이면) , ||(or ...아니면)
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-  return <></>;
+  const name = "흥민";
+  const age = 5; // or 30;
+  return (
+    <div>
+      <form action=""></form>
+      <p>name = {name}</p>
+      {name == "흥민" && <p>공격수</p>}
+      {name == "강인" && <p>미드필더</p>}
+      <hr />
+      <p>age = {age}</p>
+      {age >= 20 && <p>투표가능</p>}
+      {age < 20 && <p>투표불가능</p>}
+    </div>
+  );
 }
 
 export default App;
