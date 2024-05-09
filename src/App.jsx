0f47082ab541a305d214e7fd00c82113c5d49f53
daeseong <zang5989@gmail.com>
2024-05-09 14:48:03 +0900

    20240509 react 문법공부, javascript object
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function App(props) {
+  // object
+  // {} 안에 property 명 : property 값 쌍들이 작성 됨
+  let a = {};
+  let b = { name: "흥민" };
+  let c = { address: "신촌", city: "서울", country: "한국" };
+
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+
   return <div></div>;
 }
 
