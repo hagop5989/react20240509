d898217c5ec34793c4219ad3854239cabbd1c4b7
daeseong <zang5989@gmail.com>
2024-05-09 15:38:26 +0900

    20240509 react 문법공부, 배열의 메소드, push, pop, map
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = {};
-  console.log("a", typeof a);
-
-  const b = []; // 배열 도 객체
-  console.log("b", typeof b);
-
-  // 배열 : 여러 값을 순서(index)대로 가지고 있는 객체
-
-  const c = ["son", "lee", "kim", "hwang"];
-  console.log("c", c);
-  console.log("c-0", c[0]);
-  console.log("c-1", c[1]);
-  console.log("c-2", c[2]);
-  console.log("c-3", c[3]);
-  console.log("c-4", c[4]); // undefined
-
-  return (
-    <div>
-      <ul>
-        <li>{c[0]}</li>
-        <li>{c[1]}</li>
-        <li>{c[2]}</li>
-        <li>{c[3]}</li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
