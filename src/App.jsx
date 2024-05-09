5bedeb5910f316dc419009b55554eb6e8b64729b
daeseong <zang5989@gmail.com>
2024-05-09 15:31:25 +0900

    20240509 react 문법공부,
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
 import React from "react";
 
 function App(props) {
-  // object
-  const a = {
-    name: "John",
-  };
-
-  const b = a;
-
-  console.log(a.name);
-  console.log(b.name);
-
-  b.name = "Jane";
-
-  console.log(a.name);
-  console.log(b.name);
-
   return <div></div>;
 }
 
