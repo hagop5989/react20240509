e5fb40c117e967f4576ef41facdfb47b606a6a55
daeseong <zang5989@gmail.com>
2024-05-09 15:28:53 +0900

    20240509 react 문법공부, 자바스크립트 객체(object)도 참조값을 저장한다.
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 function App(props) {
+  // object
+  const a = {
+    name: "John",
+  };
+
+  const b = a;
+
+  console.log(a.name);
+  console.log(b.name);
+
+  b.name = "Jane";
+
+  console.log(a.name);
+  console.log(b.name);
+
   return <div></div>;
 }
 
