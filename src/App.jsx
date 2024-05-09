ff79c737a319abce13f933478d85d383514a719b
daeseong <zang5989@gmail.com>
2024-05-09 11:10:22 +0900

    20240509 react 컴포넌트 만들기 연습끝 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+function MyTag() {
+  return <h3> hello component</h3>;
+}
+
+function MyContent() {
+  return <p> my content </p>;
+}
+
 function App() {
   return (
     <>
       <div>
         <h1>Hello REACT!!</h1>
+        <MyTag />
+        <MyTag />
+
+        <MyContent />
       </div>
     </>
   );
