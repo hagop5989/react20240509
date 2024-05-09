61fc5a9e369a575f984f931c23e90d1bd7fdf10e
daeseong <zang5989@gmail.com>
2024-05-09 11:12:09 +0900

    20240509 react 필요 없는 코드 지우기 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,8 @@
 
 // props
 
-<Welcome name="" />;
-function Welcome(props) {
-  return <h1>Hello, {props.name};</h1>;
-}
-
-function MyComponent() {
-  // return : 화면에 출력할 코드
-  return <div>hello component !! </div>;
-}
-
-function MyComp() {
-  // JSX 코드 : <div></div> 이런식의 태그를 가진 코드
-  const myElem = <h1>hi JSX !</h1>;
-
-  return myElem;
-}
-
 function App() {
-  // 한줄 주석
-  /*
-   * 여러줄 주석
-   */
-
-  // component 사용 시 종료 태그 생략할 수 없음
-  return (
-    <>
-      <div>
-        <Welcome name={"react"} />
-        {/* jsx 내에서의 주석 */}
-        <h1>Hello REACT!!</h1>
-        <h2>작업중엔 ctrl + s</h2>
-        <br />
-        <MyComponent />
-        <MyComp />
-      </div>
-    </>
-  );
+  return <div> hello react</div>;
 }
 
 export default App;
