95b5c8a4ca6fe9d8962f18ee6838b7e82dbebad1
daeseong <zang5989@gmail.com>
2024-05-09 15:50:42 +0900

    20240509 react 문법공부, 배열의 메소드, push, pop, map
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 
 function App(props) {
+  // 배열
+  const a = [];
+
+  // push : 새 값을 배열 맨 뒤에 추가
+  a.push("흥민");
+  a.push("강인");
+  a.push(3.14);
+  console.log(a);
+  // pop : 맨 뒤의 값을 제거 후 리턴
+  let val1 = a.pop();
+  console.log(val1);
+  let val2 = a.pop();
+  console.log(val2);
+  console.log(a);
+
+  // map : 각 원소를 다른 값으로 매핑 (mapping) 후 새 배열 리턴
+  // 다른 값으로 매핑되는 함수를 파라미터로 제공해야 함
+  const b = [5, 4, 3, 2, 1];
+  console.log("b", b);
+
+  function action(param) {
+    return param * 2;
+  }
+
+  let c = b.map(function (param) {
+    return param * 3;
+  });
+  console.log("c", c);
   return <div></div>;
 }
 
