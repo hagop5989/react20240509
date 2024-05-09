80af1ab8112eaaed486b62e6981efbdca12b19ff
daeseong <zang5989@gmail.com>
2024-05-09 14:43:41 +0900

    20240509 react 문법공부,
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,6 @@
-function App() {
-  // javascript review
+import React from "react";
 
-  // javascript data type
-  let a; //  값을 할당하지 않으면 undefined
-  console.log("a", typeof a);
-
-  let b = "문자열 타입"; // string
-  console.log("b", typeof b);
-
-  let c = 3; // number
-  console.log("c", typeof c);
-
-  let d = 3.14; // number
-  console.log("d", typeof d);
-
-  let e = true; // boolean
-  console.log("e", typeof e);
-
-  let f = {}; // object;
-  console.log("f", typeof f);
-
-  let g = function () {}; // function
-  console.log("g", typeof g);
-
-  g = {}; // javascript 는 값이 들어갈때 타입이 정해짐 (function -> object 로 변경)
-  console.log("g", typeof g);
-
+function App(props) {
   return <div></div>;
 }
 
