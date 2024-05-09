617ca56d64002b46c9b7208d521ac3f947426459
daeseong <zang5989@gmail.com>
2024-05-09 11:22:20 +0900

    20240509 react 최상위 컴포넌트 1개 fragment
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+function MyComponent() {
+  // 최상위 컴포넌트 사용하지 않을 때 -> fragment 사용
+  return (
+    <>
+      <p>Lorem ipsum dolor.</p>
+      <p>Consequuntur molestiae, repellendus.</p>
+    </>
+  );
+}
+
+function MyComp() {
+  // 최상위 컴포넌트는 1개여야 함
+  return (
+    <div>
+      <p>Lorem ipsum.</p>
+      <p>Maxime, vel.</p>
+    </div>
+  );
+}
+
+function MyTag() {
+  // jsx 코드가 여러 줄일때 () 사용
+  return (
+    <div>
+      hello tag
+      <p>Lorem ipsum</p>
+      <p>Est, quoes?</p>
+    </div>
+  );
+}
+
 function App() {
-  return <div> hello react</div>;
+  return (
+    <div>
+      hello react
+      <MyTag />
+      <MyComp />
+      <MyComponent />
+    </div>
+  );
 }
 
 export default App;
