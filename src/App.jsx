e4c1ba070417a5847e2060a526d7a561873e2b1e
daeseong <zang5989@gmail.com>
2024-05-09 15:07:42 +0900

    20240509 react 문법공부, javascript object와 property
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,21 @@ function App(props) {
   console.log("b", b);
   console.log("c", c);
 
+  console.log("b의 name", b.name);
+  console.log("c의 name", c.name);
+
+  console.log("b의 name", b["name"]);
+  console.log("c의 address", c["address"]);
+
+  b.name = "강인";
+  console.log("b의 name", b["name"]);
+
+  // 없는 프로퍼티에도 접근 가능 (오류 아님)
+  console.log("b의 city", b.city); // undefined
+  b.city = "부산"; // 원래 없지만 있는 것처럼 줄 수 있음
+  console.log("b의 city", b.city); // city 프로퍼티 추가됨
+  console.log("b", b);
+
   return <div></div>;
 }
 
