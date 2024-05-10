d56c02427b25929c01bfa8b2938a78f683926b2d
daeseong <zang5989@gmail.com>
2024-05-10 15:37:40 +0900

    20240510 react 문법공부 - props 활용연습 완료 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import * as PropTypes from "prop-types";
+
+function MyButton(props) {
+  return (
+    <div>
+      <button onClick={props.onClick}>{props.name}</button>
+    </div>
+  );
+}
+
+MyButton.propTypes = {
+  onClick: PropTypes.func,
+  name: PropTypes.string,
+};
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton name={"샾 출력"} onClick={() => console.log("####")} />
+      <MyButton name={"느낌표 출력"} onClick={() => console.log("!!!!")} />
+    </div>
+  );
 }
 
 export default App;
