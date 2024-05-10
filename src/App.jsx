992654272e11c38e106e3c9a0092856238ac51cf
daeseong <zang5989@gmail.com>
2024-05-10 15:19:21 +0900

    20240510 react 문법공부 -
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  return <div></div>;
-}
 function App(props) {
-  // props 의 이름은 lowerCamelCase 로 작성
-  // 예약어를 쓰지 말 것 ( let={5} const ={5} ... )
-
-  return (
-    <div>
-      <label htmlFor="input1">name</label>
-      <input id="input1" type="text" />
-      <p className="error note">Lorem ipsum dolor sit amet.</p>
-      <MyComp number={3} pageNumber={3} let={5} const={5} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
