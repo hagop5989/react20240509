c18443f4510dbee749b04e725ae4bbc6965a52d4
daeseong <zang5989@gmail.com>
2024-05-10 14:22:14 +0900

    20240510 react 문법공부 -
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import React from "react";
 
-// 첫 번째 파라미터로 객체가 들어옴 (props)
-function MyComp(props) {
-  console.log(props);
-  return (
-    <div>
-      <p>name: {props.name}</p>
-      <p>age: {props.age}</p>
-      <p>address: {props.address}</p>
-      <p>city: {props.city}</p>
-      <p>country: {props.country}</p>
-    </div>
-  );
-}
-
 function App(props) {
-  // html 은 attribute react 는 property
-  return (
-    <div>
-      <MyComp name="son" age={33} address="seoul" />
-      <MyComp city="busan" country="USA" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
