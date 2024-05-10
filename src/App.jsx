5f8a6dd15c8fdac61c65ed43285ce4c0a9339886
daeseong <zang5989@gmail.com>
2024-05-10 16:31:39 +0900

    20240510 react 문법공부 - style props 응용 연습완료 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+function MyButton({ name, sx }) {
+  return (
+    <button onClick={() => console.log("clicked!!")} style={sx}>
+      {name}{" "}
+    </button>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton
+        name={"버튼1"}
+        sx={{
+          backgroundColor: "black",
+          color: "white",
+          fontSize: "3rem",
+          fontWeight: "bold",
+        }}
+      />
+      <MyButton
+        name={"버튼2"}
+        sx={{
+          backgroundColor: "lightblue",
+          color: "white",
+          fontSize: "3rem",
+          fontWeight: "bold",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
