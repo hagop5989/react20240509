bb70da2f49023c54f1d4c85cea7a2f7a0cbd203c
daeseong <zang5989@gmail.com>
2024-05-10 14:29:17 +0900

    20240510 react 문법공부 - props 를 분해할당으로 받기 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-function App(props) {
+function MyElem(props) {
+  console.log(props);
+
+  const { name, age } = props;
+  console.log(name);
+  console.log(age);
+
   return <div></div>;
+}
+
+function MyComp({ city, country, price }) {
+  console.log("city", city);
+  console.log(country);
+  console.log(price);
+  return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyElem name="son" age={33} />
+      <MyComp city="seoul" country="korea" price={500} />
+    </div>
+  );
 }
 
 export default App;
