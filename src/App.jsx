fd9962f0db7f74a2555d2f4511d1dbc8444f4fc5
daeseong <zang5989@gmail.com>
2024-05-10 15:38:32 +0900

    20240510 react 문법공부 -
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, someProp, otherProp }) {
-  console.log({ name });
-  console.log({ age });
-  console.log({ someProp }); // true : 값 없이 작성되면 true
-  console.log({ otherProp }); // true
-  return <div></div>;
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="son" age={30} someProp otherProp={true} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
