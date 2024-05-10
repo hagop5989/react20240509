d74eed377860750bae96aae89f6abb152623aa40
daeseong <zang5989@gmail.com>
2024-05-10 15:02:48 +0900

    20240510 react 문법공부 - props 의 값이 없을 때 기본값 할당하는 법 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import * as PropTypes from "prop-types";
+
+function MyButton({ name = "클릭" }) {
+  const [a, setA] = React.useState(0);
+  let count = 0;
+  function click() {
+    if (count / 2 === 0) {
+      setA("구독하기");
+    } else {
+      setA("구독취소");
+    }
+    count++;
+  }
+  return (
+    <button onClick={click}>
+      {name}
+      {a}
+    </button>
+  );
+}
+
+MyButton.propTypes = { name: PropTypes.string };
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton name="버튼1 " />
+      <MyButton />
+    </div>
+  );
 }
 
 export default App;
