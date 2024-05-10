0706251369b1e697e2097321c7efba673f55c92d
daeseong <zang5989@gmail.com>
2024-05-10 16:08:41 +0900

    20240510 react 문법공부 - style props 은 객체로 css property 명은 lowerCamelCase 로 작성 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // style props
+
+  return (
+    <div>
+      {/* style property 는 객체를 줘야함{}*/}
+      <div
+        style={{
+          color: "lightcoral",
+          padding: "10px",
+          border: "2px dotted green",
+          textAlign: "center",
+          backgroundColor: "aqua",
+        }}
+      >
+        Lorem ipsum dolor.
+      </div>
+    </div>
+  );
 }
 
 export default App;
