148977a390cdc2a43bf0718e3aa04337effebcc8
daeseong <zang5989@gmail.com>
2024-05-10 10:18:22 +0900

    20240510 react 문법공부, onClick property 와 함수 사용  연습완료 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function click1() {
+    console.log("click1");
+  }
+  const arrow = () => {
+    console.log("arrow");
+  };
+  const a = function () {
+    console.log("이름없음");
+  };
+
+  return (
+    <div>
+      <button onClick={click1}>
+        click1 onClick property에 이름 있는 함수 넣기
+      </button>
+      <button onClick={a}>click2 onClick property에 이름 없는 함수 넣기</button>
+      <button
+        onClick={function () {
+          console.log("이름없는 함수 바로 넣기");
+        }}
+      >
+        click3 onClick property에 이름 없는 함수 바로넣기
+      </button>
+      <button onClick={arrow}>
+        click4 onClick property에 arrow function 변수에 담아서 넣기
+      </button>
+      <button
+        onClick={() => {
+          console.log("arrow 바로 넣기");
+        }}
+      >
+        click5 onClick property에 arrow function 바로 넣기
+      </button>
+    </div>
+  );
 }
 
 export default App;
