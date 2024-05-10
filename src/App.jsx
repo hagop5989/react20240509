08adcefac02bb1699bb4d92bc092d9fe5aa4c0ea
daeseong <zang5989@gmail.com>
2024-05-10 11:40:43 +0900

    20240510 react 문법공부,  분해 할당의 나머지 모두 문법  (spread)으로 객체 복사 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,47 @@
 import React from "react";
 
 function App(props) {
+  // 나머지 모두 문법으로 객체 복사
+
+  const a = {
+    name: "son",
+    age: 33,
+    city: "london",
+  };
+  const b = a;
+  b.age = 44;
+  console.log("b.age", b.age);
+  console.log("a.age", a.age);
+
+  const c = {
+    name: a.name,
+    age: a.age,
+    city: a.city,
+  };
+
+  c.age = 55;
+  console.log("c.age", c.age); // 55
+  console.log("b.age", b.age); // 44
+
+  // 객체의 각 프로퍼티를 복사
+  const { ...d } = a;
+  d.age = 66;
+  console.log("d.age", d.age); // 66
+  console.log("a.age", a.age); // 44
+
+  // 연습 :
+  const e = {
+    name: "lee",
+    country: "korea",
+    team: "paris",
+    salary: 700,
+    city: "seoul",
+  };
+  // 코드 작성!
+  const { ...f } = e;
+  e.city = "busan";
+  console.log(e);
+
   return <div></div>;
 }
 
