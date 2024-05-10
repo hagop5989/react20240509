270eaa5bcfec37451584e9c5801c1c9499955866
daeseong <zang5989@gmail.com>
2024-05-10 15:23:11 +0900

    20240510 react 문법공부 - prop 에 값이 없이 이름만 작성되면 값이 true (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function App(props) {
+function MyComp({ name, age, someProp, otherProp }) {
+  console.log({ name });
+  console.log({ age });
+  console.log({ someProp }); // true : 값 없이 작성되면 true
+  console.log({ otherProp }); // true
   return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyComp name="son" age={30} someProp otherProp={true} />
+    </div>
+  );
 }
 
 export default App;
