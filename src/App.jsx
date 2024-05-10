e138044ac80200d54f6ec5ddc4c34e5f0638044f
daeseong <zang5989@gmail.com>
2024-05-10 14:37:46 +0900

    20240510 react 문법공부 - props 를 분해할당으로 받기 연습완료 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+function MyInfo({ name, team, salary, country }) {
+  console.log({});
+
+  return (
+    <div>
+      <ul>
+        <li>name : {name}</li>
+        <li>team : {team}</li>
+        <li>salary : {salary}</li>
+        <li>country : {country}</li>
+      </ul>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyInfo name="흥민" team="토트넘" salary={500} country="korea" />
+    </div>
+  );
 }
 
 export default App;
