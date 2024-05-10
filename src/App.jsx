4fc78cf4ed4e1c16f525ddfd7b0c9a328c712f7b
daeseong <zang5989@gmail.com>
2024-05-10 16:09:13 +0900

    20240510 react 문법공부 -
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // style props
-
-  return (
-    <div>
-      {/* style property 는 객체를 줘야함{}*/}
-      <div
-        style={{
-          color: "lightcoral",
-          padding: "10px",
-          border: "2px dotted green",
-          textAlign: "center",
-          backgroundColor: "aqua",
-        }}
-      >
-        Lorem ipsum dolor.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
