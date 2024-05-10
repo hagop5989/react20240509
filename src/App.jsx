b0961be8cf38ae5245a3d43b65c9a32a2bde3761
daeseong <zang5989@gmail.com>
2024-05-10 16:13:30 +0900

    20240510 react 문법공부 - style (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+function MyComp({ color }) {
+  return (
+    <div style={{ color: color }}>
+      <h1>Lorem.</h1>
+      <p>Lorem ipsum dolor.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp color:red />
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
