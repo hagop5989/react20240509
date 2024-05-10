792bb681e1435c8c153e2d982e146c12276d7793
daeseong <zang5989@gmail.com>
2024-05-10 09:48:35 +0900

    20240510 react 문법공부, onClick property 와 함수 사용 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function action1() {
+    console.log("action 1");
+  }
+
+  const action2 = function () {
+    console.log("이름없는 액션");
+  };
+
+  action1();
+  return (
+    <div>
+      <button onClick={action1}>클릭!</button>
+      <button onClick={action2}>클릭2!</button>
+      <button
+        onClick={function () {
+          console.log("anonymous action ###");
+        }}
+      >
+        클릭#
+      </button>
+    </div>
+  );
 }
 
 export default App;
