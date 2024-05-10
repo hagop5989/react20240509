36e0e8acc6b290c53df45682a0109758aa3307bf
daeseong <zang5989@gmail.com>
2024-05-10 10:40:35 +0900

    20240510 react 문법공부, 구조분해 할당 연습완료 (destructuring assignment) (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 function App(props) {
+  const person = {
+    email: "test@example.com",
+    name: "test",
+    address: "강남",
+    country: "usa",
+  };
+
+  const { email, name, address, country } = person;
+
+  console.log("email: ", email);
+  console.log("name: ", name);
+  console.log("address: ", address);
+  console.log("country: ", country);
+
   return <div></div>;
 }
 
