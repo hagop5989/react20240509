36d17fe48c3b95c99c0b9cde23d5c3c30f295caf
daeseong <zang5989@gmail.com>
2024-05-13 10:32:28 +0900

    20240510 react - 문법공부 react component 를 만드는 3가지 방법 - 이름 있는 함수 - 이름 없는 함수 - arrow function (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ArrowComp, MyComp, Yourcomp } from "./MyComps.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+      <Yourcomp />
+      <ArrowComp />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyComps.jsx
@@ -0,0 +1,11 @@
+// react component 들 (대문자로 시작!)
+
+export function MyComp() {
+  return null;
+}
+
+export const Yourcomp = function() {
+  return <div>yourcomp</div>;
+};
+
+export const ArrowComp() => <div>arrow function componet</div>;
