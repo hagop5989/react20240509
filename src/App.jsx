2510f473514b84b641902514ab3d1a33e2654396
daeseong <zang5989@gmail.com>
2024-05-13 15:35:19 +0900

    20240513 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
-
-  function handleClick() {
-    list.push(text);
-    const [...nextList] = list; // 얕은복사
-    nextList.push(text);
-    setList(nextList);
-    setText("");
-  }
-
-  return (
-    <div>
-      <input
-        value={text}
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleClick}>추가</button>
-      <ul>
-        {list.map((item) => {
-          <li>{item}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
