d25add50b4c6c7908b07fc658ba51421325310f3
daeseong <zang5989@gmail.com>
2024-05-13 09:12:41 +0900

    20240510 react chakra ui 맛보기 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Button,
+  ChakraProvider,
+  Progress,
+  Stack,
+} from "@chakra-ui/react";
 
 function App(props) {
   return (
@@ -21,6 +30,48 @@ function App(props) {
           click3{" "}
         </Button>
       </div>
+
+      <div>
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>Your browser is outdated!</AlertTitle>
+          <AlertDescription>
+            Your Chakra experience may be degraded.
+          </AlertDescription>
+        </Alert>
+      </div>
+      <div>
+        <br />
+      </div>
+      <Stack spacing={5}>
+        <Progress colorScheme="green" size="sm" value={20} />
+        <Progress colorScheme="green" size="md" value={20} />
+        <Progress colorScheme="green" size="lg" value={20} />
+        <Progress colorScheme="green" height="32px" value={20} />
+      </Stack>
+      <br />
+      <Progress
+        colorScheme={"red"}
+        sx={{ height: "50px" }}
+        size="xs"
+        isIndeterminate
+      />
+
+      <Alert
+        status="success"
+        variant="subtle"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        height="200px"
+      >
+        <AlertIcon boxSize="40px" mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize="lg">
+          전송 완료 되었습니다!!
+        </AlertTitle>
+        <AlertDescription maxWidth="sm">abcccccccccccccccc</AlertDescription>
+      </Alert>
     </ChakraProvider>
   );
 }
