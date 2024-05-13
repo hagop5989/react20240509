0eafea19f52f9584a3bf5d8465a302c8c56bbe58
daeseong <zang5989@gmail.com>
2024-05-13 16:15:08 +0900

    20240513 react- 공부  상태예제6) 하나의 state(객체) 가 여러 property 를 갖는 경우 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [person, setPerson] = useState({
+    name: "",
+    address: "",
+    city: "",
+    email: "",
+  });
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            // person.name = e.target.value;
+            const { ...nextPerson } = person;
+            nextPerson.name = e.target.value;
+            setPerson(nextPerson);
+          }}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            const { ...nextPerson } = person;
+            nextPerson.email = e.target.value;
+            setPerson(nextPerson);
+          }}
+          placeholder={"이메일"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            const nextPerson = { ...person, city: e.target.value };
+            setPerson(nextPerson);
+          }}
+          placeholder={"도시"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPerson({ ...person, address: e.target.value });
+          }}
+          placeholder={"주소"}
+        />
+      </div>
+      <ul>
+        <li>name : {person.name}</li>
+        <li>email : {person.email}</li>
+        <li>city : {person.city}</li>
+        <li>address : {person.address}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
