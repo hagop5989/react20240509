5f8da61e4efe0ce4bb82629b250b0c4c57def8b2
daeseong <zang5989@gmail.com>
2024-05-14 14:11:12 +0900

    20240514 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
-
-// step : 컨텍스트 만들기
-let TextContext = createContext(null);
-
-function MyInput() {
-  const textHandler = useContext(TextContext);
-  return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => textHandler.updateText(e.target.value)}
-      />
-      <p>MyInput : {textHandler.text}</p>
-    </div>
-  );
-}
-
-function MyText() {
-  const textHandler2 = useContext(TextContext);
-  return (
-    <div>
-      <p>MyText : {textHandler2.text}</p>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-
-  function handleUpdateText(t) {
-    setText(t);
-  }
-
-  return (
-    <div>
-      <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
-        <MyInput />
-        <MyText />
-      </TextContext.Provider>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
