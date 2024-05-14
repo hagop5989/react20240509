f778d8f43d87f7c5dd77701289e35d8405430a5a
daeseong <zang5989@gmail.com>
2024-05-14 09:46:21 +0900

    20240514 react- 공부  트리구조로 구성되고 컴포넌트가 트리에서 떨어진 후 다시 붙으면 initial rendering 됨 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
-function MyComp() {
+function MyComp({ color }) {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div style={{ border: `5px solid ${color}` }}>
       <div>{count}</div>
       <div>
         <button onClick={() => setCount(count + 1)}>UP</button>
@@ -14,10 +14,24 @@ function MyComp() {
 }
 
 function App(props) {
+  const [view1, setView1] = useState();
+  const [view2, setView2] = useState();
   return (
     <div>
-      <MyComp />
-      <MyComp />
+      <input
+        type={"checkbox"}
+        checked={view1}
+        onClick={(e) => setView1(e.target.checked)}
+      />
+
+      {view1 && <MyComp color={"blue"} />}
+      <hr />
+      <input
+        type={"checkbox"}
+        checked={view2}
+        onClick={(e) => setView2(e.target.checked)}
+      />
+      {view2 && <MyComp color={"red"} />}
     </div>
   );
 }
