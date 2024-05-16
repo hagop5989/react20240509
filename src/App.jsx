c884e677e35366fe55f4407331edc7d8b6a2dae0
daeseong <zang5989@gmail.com>
2024-05-16 11:41:26 +0900

    20240516 react- 공부  요청처리 메소드 작성 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
   function handleClick1() {
     const obj1 = { name: "son", age: 10 };
     // const json1 = JSON.stringify(obj1); // 자동으로 됨
@@ -10,9 +12,38 @@ function App(props) {
     axios.post("/api/main41/sub1", obj1);
   }
 
+  function handleClick2() {
+    const obj2 = {
+      time: new Date().toLocaleTimeString(),
+      day: new Date().toLocaleString("ko", { weekday: "long" }),
+    };
+    axios.post("/api/main41/sub2", obj2);
+  }
+
+  function handleClick3() {
+    // const data = {
+    //   name : name,
+    //   city : city,
+    // };
+    // const data = {
+    //   name ,
+    //   city ,
+    // }
+    axios.post("/api/main41/sub3", { name, city });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>요청 (json 데이터 포함)</button>
+      <button onClick={handleClick2}>요청2 (json 데이터 포함)</button>
+      {/*<div>(input+br)*2+button</div>*/}
+      <div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <br />
+        <input type="text" onChange={(e) => setCity(e.target.value)} />
+        <br />
+        <button onClick={handleClick3}>요청 (input 에 입력한 값)</button>
+      </div>
     </div>
   );
 }
