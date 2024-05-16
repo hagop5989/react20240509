87dc0f0371b6e47af9ee7804e3e169502af4ad60
daeseong <zang5989@gmail.com>
2024-05-16 16:05:07 +0900

    20240516 react- 공부 http응답 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,38 @@ import React from "react";
 import axios from "axios";
 
 function App(props) {
+  function handleClick200() {
+    axios.get("/api/main43/sub1").then((response) => {
+      console.log("200응답");
+    });
+  }
+
+  function handleClick400() {
+    axios
+      .get("/api/main43/sub2")
+      .then((response) => console.log("400응답"))
+      .catch(() => console.log("400응답 catch"));
+  }
+
+  function handleClick404() {
+    axios
+      .get("/api/main43/sub5")
+      .then()
+      .catch(() => console.log("404응답"));
+  }
+  function handleClick500() {
+    axios
+      .get("/api/main43/sub6")
+      .then()
+      .catch(() => console.log("500응답"));
+  }
+
   return (
     <div>
-      <button onClick={() => axios.get("/api/main43/sub1")}>200 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub2")}>400 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub3")}>401 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub4")}>403 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub5")}>404 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub6")}>500 응답</button>
+      <button onClick={handleClick200}>200 응답</button>
+      <button onClick={handleClick400}>400 응답</button>
+      <button onClick={handleClick404}>404 응답</button>
+      <button onClick={handleClick500}>500 응답</button>
     </div>
   );
 }
