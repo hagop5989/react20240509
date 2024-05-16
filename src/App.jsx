7a05e5c8104b84ea45d9d30410e23d3d2ffc5d36
daeseong <zang5989@gmail.com>
2024-05-16 14:29:29 +0900

    20240516 react- 공부 JSON 응답 파싱 하기 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [result, setResult] = useState();
   function handleClick1() {
     axios.get("/api/main42/sub1").then((param) => console.log(param.data));
   }
 
   function handleClick2() {
-    axios.get("/api/main42/sub2").then((param) => console.log(param.data));
+    axios.get("/api/main42/sub2").then((param) => setResult(param.data));
+  }
+
+  function handleClick3() {
+    axios.get("/api/main42/sub3").then((res) => console.log(res.data));
   }
 
   return (
     <div>
       <button onClick={handleClick1}>응답 받기</button>
       <br />
-      <button onClick={handleClick2}>가위바위보</button>
+      <button onClick={handleClick2}>가위,바위,보</button>
+      {result}
+      <button onClick={handleClick3}>버튼3</button>
     </div>
   );
 }
