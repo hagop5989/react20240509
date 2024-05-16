d569d86729801129e54af39001d90e43e3d176a3
daeseong <zang5989@gmail.com>
2024-05-16 14:03:02 +0900

    20240516 react- 공부  js 객체가 JSON 으로 직렬화 됨  이 때 각 데이터 타입이 사용됨2 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,28 @@ function App(props) {
     axios.post("/api/main41/sub5", data);
   }
 
+  function handleClick2() {
+    axios.post("/api/main41/sub6", {
+      name: "son",
+      age: 33,
+    });
+  }
+
+  function handleClick3() {
+    axios.post("/api/main41/sub7", {
+      day: new Date().toLocaleString("ko", { weekday: "long" }),
+      time: new Date().toLocaleTimeString(),
+    });
+  }
+
   // string , number , boolean , object , array , null
   return (
     <div>
       <button onClick={handleClick1}>json 데이터와 요청</button>
+      <br />
+      <button onClick={handleClick2}>json 데이터와 요청2</button>
+      <br />
+      <button onClick={handleClick3}>json 데이터와 요청3</button>
     </div>
   );
 }
