343e235def17cda9f6257c345b6c2c9ae0b7e770
daeseong <zang5989@gmail.com>
2024-05-20 14:12:26 +0900

    20240520 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text1, setText1] = useState("");
-  const [text2, setText2] = useState("hello");
-  const [text3, setText3] = useState("hello2");
-  const [text4, setText4] = useState("안녕");
-  const [text5, setText5] = useState("올라");
-
-  return (
-    <div>
-      <div>
-        <input onChange={(e) => setText1(e.target.value)} type="text" />
-      </div>
-      {text1}
-      <div>
-        <input type="text" value={text2} readOnly={true} /> {/* 변경 불가 */}
-      </div>
-      <div>
-        <input type="text" defaultValue={text3} />
-      </div>
-      <div>
-        <input
-          type="text"
-          value={text4}
-          onChange={(e) => setText4(e.target.value)}
-        />
-      </div>
-      <div>
-        <input type="text" value={text5} />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
