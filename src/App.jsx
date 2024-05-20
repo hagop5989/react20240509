158543378d1cfa896c757a591d9b83dd78a3b688
daeseong <zang5989@gmail.com>
2024-05-20 11:39:14 +0900

    20240520 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import React from "react";
-import { Button, ChakraProvider, useToast } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  const toast = useToast();
-  return (
-    <ChakraProvider>
-      <Button
-        onClick={() => {
-          toast({
-            title: "경고 메세지",
-            description: " 경고 : XXX 문제가 발생하였습니다.",
-            position: "bottom-left",
-            status: "warning",
-            colorScheme: "red",
-            isClosable: false,
-            duration: 500,
-          });
-        }}
-      >
-        경고메세지
-      </Button>
-    </ChakraProvider>
-  );
+  return <ChakraProvider></ChakraProvider>;
 }
 
 export default App;
