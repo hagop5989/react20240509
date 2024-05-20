aeef1ac1ef7e4b15395f563c590a11fffc3bdd93
daeseong <zang5989@gmail.com>
2024-05-20 15:11:32 +0900

    20240520 react- 공부 file 전송 시 method=post encType={"multipart/form-data"} 해야함. (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/* 파일 전송 시 : method={"post"} encType={"multipart/form-data"} 를 form 에 반드시 설정 해야만 함.*/}
+      <form action="/path" method={"post"} encType={"multipart/form-data"}>
+        이름 <input type="text" name="name" />
+        <br />
+        <input type="file" name={"file"} />
+        {/* get 으로 보내명 파일 명만 전송됨 */}
+        <br />
+        <input type="submit" value="제출" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
