9d7c6250805ca49b776f4ba3ee4ba54cdfb67bfb
daeseong <zang5989@gmail.com>
2024-05-20 11:27:25 +0900

    20240520 react- 공부 모달 연습 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,35 @@
 import React from "react";
 import {
-  Box,
-  Center,
+  Button,
   ChakraProvider,
-  Checkbox,
-  FormControl,
-  FormLabel,
-  Input,
-  Radio,
-  Switch,
-  Textarea,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 function App(props) {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
     <ChakraProvider>
-      <label htmlFor={"input1"}>이름</label>
-      <input id={"input1"} />
-      <hr />
-      <Center>
-        <Box w={{ base: "100%", md: "50%" }}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-          </FormControl>
-        </Box>
-      </Center>
-      <hr />
-      <Checkbox>체크박스</Checkbox>
-      <Checkbox>Lorem.</Checkbox>
-      <Checkbox>Illo.</Checkbox>
-      <Checkbox>Vero?</Checkbox>
-      <hr />
-      <Radio value={1}>radio1</Radio>
-      <Radio value={2}>radio2</Radio>
-      <Radio value={3}>radio3</Radio>
-      <hr />
-      <Switch>스위치</Switch>
-      <hr />
-      <Textarea>Textarea 입니다.</Textarea>
+      <Button onClick={onOpen}>모달 오픈 버튼!</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader>모달 헤더 입니다 </ModalHeader>
+            <ModalBody>모달 바디 입니다 </ModalBody>
+            <ModalFooter>
+              모달 푸터 입니다
+              <Button onClick={onClose}>닫기</Button>
+              <Button colorScheme={"red"}>저장</Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
     </ChakraProvider>
   );
 }
