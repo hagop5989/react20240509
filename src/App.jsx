af4580edf53e7b23fad1720eb25598435d0a5471
daeseong <zang5989@gmail.com>
2024-05-20 12:35:26 +0900

    20240520 react- 공부   react hook(use...) 은 컴포넌트 상단에 if/for 문 없이 순서대로 작성할 것. (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { ChakraProvider, useDisclosure, useToast } from "@chakra-ui/react";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  const [count, setCount] = useState(0);
+  useEffect(() => {}, []);
+  const toast = useToast();
+  const { onOpen, isOpen, onClose } = useDisclosure();
+
+  // use... : hook 이라 함.
+  // if / for 내에서 사용하지 말 것
+
+  if (true) {
+    const [text, setText] = useState(""); /* 실행 될 수도 안 될수도 */
+  }
+  const [message, setMessage] =
+    useState(""); /* 위의 hook 이 실행이 안되면 순서가 꼬임 */
+
+  return (
+    <ChakraProvider>
+      <div></div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
