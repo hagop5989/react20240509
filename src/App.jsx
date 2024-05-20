d878d87de71a9f49f5ada9ee5cb384cc254f86b5
daeseong <zang5989@gmail.com>
2024-05-20 10:22:30 +0900

    20240520 react- 공부  hover pseudo (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <Box bgColor={"orange"} _hover={{ bgColor: "lightblue" }}>
+      Lorem ipsum dolor.
+    </Box>
+  );
 }
 
 export default App;
