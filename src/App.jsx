450055dbffef37733a2e456a18769c9af4e91ef9
daeseong <zang5989@gmail.com>
2024-05-20 11:33:04 +0900

    20240520 react- 공부  toast (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
 import React from "react";
-import {
-  Button,
-  ChakraProvider,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Button, ChakraProvider, useToast } from "@chakra-ui/react";
 
 function App(props) {
-  const { isOpen, onClose, onOpen } = useDisclosure();
-
+  const toast = useToast();
   return (
     <ChakraProvider>
-      <Button onClick={onOpen}>모달 오픈 버튼!</Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay>
-          <ModalContent>
-            <ModalHeader>모달 헤더 입니다 </ModalHeader>
-            <ModalBody>모달 바디 입니다 </ModalBody>
-            <ModalFooter>
-              모달 푸터 입니다
-              <Button onClick={onClose}>닫기</Button>
-              <Button colorScheme={"red"}>저장</Button>
-            </ModalFooter>
-          </ModalContent>
-        </ModalOverlay>
-      </Modal>
+      <Button
+        onClick={() => {
+          toast({
+            title: "토스트 제목",
+            description: "토스트 내용",
+            position: "top",
+            status: "info" /*정보 상태 */,
+            isClosable: true,
+            duration: 1000,
+          });
+        }}
+      >
+        toast
+      </Button>
     </ChakraProvider>
   );
 }
