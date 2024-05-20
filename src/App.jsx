af2f66bf0b7af6306f7fef3b684827b2586ce254
daeseong <zang5989@gmail.com>
2024-05-20 14:19:51 +0900

    20240520 react- 공부  bubling (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  function handleButtonClick(e) {
+    // event bubbling 멈추기
+    e.stopPropagation();
+    console.log("버튼 클릭 됨");
+  }
+
+  function handleBoxClick() {
+    console.log("박스 클릭 됨");
+  }
+
+  return (
+    <ChakraProvider>
+      {/* 버튼을 클릭해도 박스 클릭됨이 뜸*/}
+      <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
+        <Button onClick={handleButtonClick}>클릭1</Button>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
