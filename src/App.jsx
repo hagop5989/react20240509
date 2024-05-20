ad4a22e87202c1ebeb63d2fdd781942fe29e9a85
daeseong <zang5989@gmail.com>
2024-05-20 10:44:57 +0900

    20240520 react- 공부  FormControl (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <label htmlFor={"input1"}>이름</label>
+      <input id={"input1"} />
+      <hr />
+      <Center>
+        <Box>
+          <FormControl w={{ base: "100%", md: "50%" }}>
+            <FormLabel>이름</FormLabel>
+            <Input />
+          </FormControl>
+        </Box>
+      </Center>
+    </ChakraProvider>
+  );
 }
 
 export default App;
