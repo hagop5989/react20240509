74ac5e19f5f32e7a2f3dbba72f27d120259177ff
daeseong <zang5989@gmail.com>
2024-05-20 15:03:44 +0900

    20240520 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import React from "react";
 
 function App(props) {
-  function handleLinkClick(e) {
-    // 기본 이벤트 동작 막기
-    e.preventDefault();
-
-    // 개발자가 원하는 동작...
-  }
-
-  function handleSubmitClick(e) {
-    e.preventDefault();
-  }
-
-  return (
-    <div>
-      <a onClick={handleLinkClick} href="https://www.naver.com">
-        네이버
-      </a>
-      <hr />
-      <form action={"https://www.naver.com"} onSubmit={handleSubmitClick}>
-        <input type="text" />
-        <button>전송</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
