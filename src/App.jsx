58308b28337391bd8fcd98d77d3da8bc4010b300
daeseong <zang5989@gmail.com>
2024-05-20 11:10:39 +0900

    20240520 react- 공부  FormComponent 들 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import {
   Box,
   Center,
   ChakraProvider,
+  Checkbox,
   FormControl,
   FormLabel,
   Input,
+  Radio,
+  Switch,
+  Textarea,
 } from "@chakra-ui/react";
 
 function App(props) {
@@ -15,13 +19,26 @@ function App(props) {
       <input id={"input1"} />
       <hr />
       <Center>
-        <Box>
-          <FormControl w={{ base: "100%", md: "50%" }}>
+        <Box w={{ base: "100%", md: "50%" }}>
+          <FormControl>
             <FormLabel>이름</FormLabel>
             <Input />
           </FormControl>
         </Box>
       </Center>
+      <hr />
+      <Checkbox>체크박스</Checkbox>
+      <Checkbox>Lorem.</Checkbox>
+      <Checkbox>Illo.</Checkbox>
+      <Checkbox>Vero?</Checkbox>
+      <hr />
+      <Radio value={1}>radio1</Radio>
+      <Radio value={2}>radio2</Radio>
+      <Radio value={3}>radio3</Radio>
+      <hr />
+      <Switch>스위치</Switch>
+      <hr />
+      <Textarea>Textarea 입니다.</Textarea>
     </ChakraProvider>
   );
 }
