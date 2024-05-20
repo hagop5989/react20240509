a3ffdb8ca1c1ea60043617b31e50ab2a200e15db
daeseong <zang5989@gmail.com>
2024-05-20 10:19:13 +0900

    20240520 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Box bgColor={"tomato"} w={"50%"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box bgColor={"teal"} w={"768px"}>
-        768px
-      </Box>
-      <Box bgColor={"lightcoral"} w={{ base: "100%", md: "500px" }}>
-        768을 넘으면 500px, 그 전엔 100%
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
