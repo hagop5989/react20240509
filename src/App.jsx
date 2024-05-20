8137a4d8b54c11f1fd67eaa103dd8c62c63db48e
daeseong <zang5989@gmail.com>
2024-05-20 10:18:59 +0900

    20240520 react- 공부  반응형 스타일1 (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <div></div>
+      <Box bgColor={"tomato"} w={"50%"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box bgColor={"teal"} w={"768px"}>
+        768px
+      </Box>
+      <Box bgColor={"lightcoral"} w={{ base: "100%", md: "500px" }}>
+        768을 넘으면 500px, 그 전엔 100%
+      </Box>
     </ChakraProvider>
   );
 }
