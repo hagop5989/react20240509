e1b873d2cbda55dc9c82f8a5dc4766019bdd0b1f
daeseong <zang5989@gmail.com>
2024-05-20 14:29:08 +0900

    20240520 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from "react";
-import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  function handleButtonClick(e) {
-    // event bubbling 멈추기
-    e.stopPropagation();
-    console.log("버튼 클릭 됨");
-  }
-
-  function handleBoxClick() {
-    console.log("박스 클릭 됨");
-  }
-
-  return (
-    <ChakraProvider>
-      {/* 버튼을 클릭해도 박스 클릭됨이 뜸*/}
-      <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
-        <Button onClick={handleButtonClick}>클릭1</Button>
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
