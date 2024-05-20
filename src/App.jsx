780f1f41952f1977c4cc6ec9827fbaf7814a1055
daeseong <zang5989@gmail.com>
2024-05-20 10:23:26 +0900

    20240520 react- 공부
    (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <Box bgColor={"orange"} _hover={{ bgColor: "lightblue" }}>
-      Lorem ipsum dolor.
-    </Box>
-  );
+  return <div></div>;
 }
 
 export default App;
