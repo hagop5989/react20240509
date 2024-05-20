919617cfb7d14f815d382b957b18562b63f7699a
daeseong <zang5989@gmail.com>
2024-05-20 14:34:01 +0900

    20240520 react- 공부 preventDefault (학원)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function handleLinkClick(e) {
+    // 기본 이벤트 동작 막기
+    e.preventDefault();
+
+    // 개발자가 원하는 동작...
+  }
+
+  function handleSubmitClick(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <a onClick={handleLinkClick} href="https://www.naver.com">
+        네이버
+      </a>
+      <hr />
+      <form action={"https://www.naver.com"} onSubmit={handleSubmitClick}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
